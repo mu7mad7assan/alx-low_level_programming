7b9ec9ace15567e63178decfe0b0b3fe6041c708
mu7mad7assan <mu7mad.7assan@gmail.com>
2023-03-21 16:53:09 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function add two values
+ * @n1: the first parameter
+ * @n2: the second parameter
+ *
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
